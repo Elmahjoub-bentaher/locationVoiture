08ca87b8fc96f7c9310d227f6ab3105c5aef94b3
Elmahjoub-bentaher <elmahjoub.bentaher@edu.uiz.ac.ma>
2024-07-29 15:23:28 +0100

    first commit

---
+++ database/migrations/2024_07_28_153847_create_voitures_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('voitures', function (Blueprint $table) {
+            $table->id();
+            $table->string('marque');
+            $table->string('modèle');
+            $table->string('année');
+            $table->string("numéro_d'immatriculation")->unique();
+            $table->string('vin')->unique();
+            $table->string('couleur');
+            $table->string('kilométrage');
+            $table->string('statut');
+            $table->string('tarif_de_location_quotidien');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('voitures');
+    }
+};
